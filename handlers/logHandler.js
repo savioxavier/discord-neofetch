be6cc2bab32c440913541a8026208bd33d45493a
Skyascii <savioxavier112@gmail.com>
2022-06-14 18:52:34 +0400

    feat: remove `pino-pretty` and use `pino-colada`

--- handlers/logHandler.js
+++ handlers/logHandler.js
@@ -1,14 +1,22 @@
-import { Chalk } from 'chalk';
+// import { Chalk } from 'chalk';
 
-const chalk = new Chalk({ level: 2 });
+// const chalk = new Chalk({ level: 2 });
 
-const logger = {
-  info: (message) => {
-    console.log(chalk.green(message));
-  },
-  error: (message) => {
-    console.error(chalk.red(message));
-  },
-};
+// const logger = {
+//   info: (message) => {
+//     console.log(chalk.green(message));
+//   },
+//   error: (message) => {
+//     console.error(chalk.red(message));
+//   },
+// };
+
+// export default logger;
+
+import pino from 'pino';
+
+const logger = pino({
+  prettifier: await import('pino-colada'),
+});
 
 export default logger;
