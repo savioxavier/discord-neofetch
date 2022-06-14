19bc7324ec0ccb077bb7c4a8b8eff8e34c1425eb
Skyascii <savioxavier112@gmail.com>
2022-06-14 18:53:24 +0400

    chore: remove unwanted comment

--- handlers/logHandler.js
+++ handlers/logHandler.js
@@ -1,18 +1,3 @@
-// import { Chalk } from 'chalk';
-
-// const chalk = new Chalk({ level: 2 });
-
-// const logger = {
-//   info: (message) => {
-//     console.log(chalk.green(message));
-//   },
-//   error: (message) => {
-//     console.error(chalk.red(message));
-//   },
-// };
-
-// export default logger;
-
 import pino from 'pino';
 
 const logger = pino({
